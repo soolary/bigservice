fdf31282737e9ba835d805a1a2fb9556589aabcd
wangjie <770499571@qq.com>
2020-06-29 12:14:28 +0800

    完成用户模块

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const koa = require('koa');
 const bodyParser = require('koa-bodyparser')
 const koaJWT = require('koa-jwt')
+const errorHandler = require('./routers/error')
 const Router = require('koa-router')
 const router = new Router()
 const app = new koa();
+errorHandler(app)//错误处理
 const cors = require('koa2-cors');
-app.use(cors());
+app.use(cors({
+    // origin: '*',
+    // maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+    // credentials: true, //是否允许发送Cookie
+    // allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
+    // allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-requested-with'], //设置服务器支持的所有头信息字段
+    // exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
+}));
 app.use(koaJWT({ secret: 'bigevent' }).unless({ path: /^\/api/ }))
 
 // app.use(async (ctx, next) => {
@@ -23,7 +32,6 @@ router.use('/api', loginRouter.routes())
 router.use('/my/article', cetegroyRouter.routes());
 router.use('/my/article', articleyRouter.routes());
 router.use('/my', userRouter.routes());
-
 app.use(router.routes())
     .use(router.allowedMethods);
 app.listen(3007, () => {
--- routers/user.js
+++ routers/user.js
@@ -1,12 +1,12 @@
 const Router = require('koa-router');
 const db = require('../db');
 const router = new Router()
+const utility = require('utility')
 //更新用户信息——————————————————
 router.get('/userinfo', async (ctx, next) => {
     // console.log(ctx.state.user);
     let sql = 'select * from user where id=?';
     let r = await db(sql, ctx.state.user.id)
-    // console.log(r);
     if (r && r.length > 0) {
         ctx.body = ({ status: 0, message: '获取用户信息成功', data: r[0] })
     } else {
@@ -26,5 +26,36 @@ router.post('/userinfo', async ctx => {
 
 
 //重置密码——————————————————————
+router.post('/updatepwd', async (ctx) => {
+
+
+    let oldPwd = utility.md5(ctx.request.body.oldPwd)
+    let newPwd = utility.md5(ctx.request.body.newPwd)
+
+
+    let r1 = await db('select * from user where password=?and id =?', [oldPwd, ctx.state.user.id])
+    if (r1 && r1.length > 0) {
+        let r2 = await db('update user set password=?where id=?', [newPwd, ctx.state.user.id]);
+        if (r2 && r2.affectedRows > 0) {
+            ctx.body = { status: 0, message: '更新密码成功' }
+        } else {
+            ctx.body = { status: 1, message: '更新密码失败' }
+        }
+    } else {
+        ctx.body = { status: 1, message: '原密码错误' }
+    }
+
+})
 //更换头像——————————————————————
+router.post('/update/avatar', async ctx => {
+    let sql = 'update user set user_pic =? where id=?';
+    let r = await db(sql, [ctx.request.body.avatar, ctx.state.user.id]);
+
+
+    if (r && r.affectedRows > 0) {
+        ctx.body = { status: 0, message: '更新头像成功' };
+    } else {
+        ctx.body = { status: 1, message: '更新头像失败' }
+    }
+})
 module.exports = router
